018c833aa5100cba8c313f3a3b508e081b226ca8
Курушина Дарья <kurushinadd@115c14.occ.local>
2023-10-14 09:18:41 +0400

    first commit

---
+++ database/migrations/2023_10_14_050703_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increaments('id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('description')->nullable();
+            $table->string('amount');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
